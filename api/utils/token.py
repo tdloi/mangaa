4e1718c36dd2d846808651247c9c29b6e29e2dd1
Loi Tran <tdloi@protonmail.com>
2019-03-05 01:31:51 +0700

    Update token utils
    Seperate send request part to able pass custom_token in case
    there is an initialized firebase instance

--- api/utils/token.py
+++ api/utils/token.py
@@ -1,3 +1,5 @@
+""" Helper function to create token for local development and testing
+"""
 import os
 
 import firebase_admin
@@ -5,30 +7,48 @@ import requests
 from firebase_admin import auth, credentials
 
 
-def create_custom_token_from_uid(uid, service_account_path=None):
-    """ Generate a custom token using service account json file
+def create_custom_token(uid, claim=None, service_account_path=None):
+    """
+    Generate a custom token using service account json file
+
+    claim: a dictionary contains attribute to add to user id_token
+    service_account_path: path contains service account json file, default on
+    `data/serviceAccountKey.json` from project root
     """
     if not service_account_path:
         service_account_path = 'data/serviceAccountKey.json'
     cred = credentials.Certificate(service_account_path)
     default_app = firebase_admin.initialize_app(cred)
-    custom_token = auth.create_custom_token(uid)
+    custom_token = auth.create_custom_token(uid, developer_claims=claim)
     firebase_admin.delete_app(default_app)
     return custom_token.decode('utf-8')
 
 
-def create_id_token(uid, service_account_path=None):
-    """ 
-    Send created custom token to Google API in exchange for
-    signed token, this token is same as token recieved from firebase
-    when user log in
+def exchange_custom_token(custom_token):
     """
-    custom_token = create_custom_token_from_uid(uid, service_account_path) 
+    Send created custom token to Google API to exchange for signed token,
+    this token is the same as token recieved from firebase when user logged in.
+    """
     body = {
         "token": custom_token,
         "returnSecureToken": True
     }
     url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key="
     key = os.environ.get('REACTE_APP_FIREBASE_APIKEY')
-    token = requests.post(f'{url}{key}', data=body).json()    
-    return token['idToken']
+    token = requests.post(f'{url}{key}', data=body).json()
+    return token
+
+
+def create_token(uid, claim=None, service_account_path=None):
+    """
+    Wrapper function to send created custom token from an uid to Google API, will
+    raise error if there is an initialized app
+
+    If uid does not exist, Firebase will create one. To know if a new user is
+    created, check isNewUser field in token
+    If claim is existed, existed claim will be override with new claim.
+    Fore more info:
+    https://firebase.google.com/docs/auth/admin/custom-claims#set_and_validate_custom_user_claims_via_the_admin_sdk
+    """
+    custom_token = create_custom_token(uid, claim, service_account_path)
+    return exchange_custom_token(custom_token)
