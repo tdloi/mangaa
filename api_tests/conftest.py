e3d0e28dbee1ef669c8ef26176e0cae4a53b1aeb
Loi Tran <tdloi@protonmail.com>
2019-03-13 06:28:46 +0700

    Put client under app_context and add endpoint fixture

--- api_tests/conftest.py
+++ api_tests/conftest.py
@@ -1,11 +1,20 @@
+from os import environ
+
 import pytest
 
 from api import create_app
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def client():
     app = create_app('test')
-    client = app.test_client()
+    client = app.test_client('test')
 
-    yield client
+    with app.app_context():
+        yield client
+
+
+@pytest.fixture
+def endpoint():
+    endpoint = environ.get('API_ENDPOINT', '/api/v1')
+    yield endpoint
