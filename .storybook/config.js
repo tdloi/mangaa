35b904ef27f1feff23fd0c3e7c39103e9447af15
Loi Tran <tdloi@protonmail.com>
2019-03-22 23:21:16 +0700

    Hide storybook addon pannel as default

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,15 @@
-import { configure } from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
 
 const req = require.context('../src/components', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  options: {
+    showPanel: false
+  }
+})
 
 configure(loadStories, module);
