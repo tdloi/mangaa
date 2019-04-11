b7ee4c72c3d42342913f4d2c2448ea30e76aa4cb
Loi Tran <tdloi@protonmail.com>
2019-04-11 16:13:20 +0700

    Add customObserver to force re-request for useFetch hook

--- src/hooks/useFetchDataApi.js
+++ src/hooks/useFetchDataApi.js
@@ -5,7 +5,7 @@ import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only';
 // update to use AbortController instead of didCancel
 // this is also an alias so don't need to append
 // REACT_APP_API for each url request
-export default function useFetchDataApi(initialUrl, customField) {
+export default function useFetchDataApi(initialUrl, customField, customObserver) {
   const host = process.env.REACT_APP_API || '';
   const url = host + initialUrl;
   const [data, setData] = useState(null);
@@ -35,7 +35,7 @@ export default function useFetchDataApi(initialUrl, customField) {
     fetchData();
 
     return () => controller.abort();
-  }, [url]);
+  }, [url, customObserver]);
 
   return [data, isLoading, isError];
 }
