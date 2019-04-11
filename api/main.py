473c9c1f2ff56e5f4a30dd78089fb2a9e6d09149
Loi Tran <tdloi@protonmail.com>
2019-04-11 15:58:42 +0700

    Add Access-Control-Allow-Headers for response with Authorization headers

--- api/main.py
+++ api/main.py
@@ -11,4 +11,5 @@ def add_cors(response):
     # TODO: Multiple CORS
     header = response.headers
     header['Access-Control-Allow-Origin'] = os.environ.get('FLASK_CORS')
+    header['Access-Control-Allow-Headers'] = 'Authorization'
     return response
