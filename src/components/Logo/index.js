9c06fea27922f78baf088bf20982262417a5b910
Loi Tran <tdloi@protonmail.com>
2019-04-21 18:50:31 +0700

    Update Logo style directly over react router Link

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const StyledLogo = styled.span`
+const StyledLogo = styled(Link)`
+  cursor: pointer;
   display: inline-block;
   text-align: center;
-  cursor: pointer;
+  text-decoration: none;
   font-weight: bold;
   font-size: 1.25rem;
   padding: 0.2rem 0.5rem;
+  &, :active, :focus {
+    color: ${props => props.theme.fg};
+    text-shadow: 2px 1px 3px ${props => props.theme.fgAlt};
+    outline: none;
+  }
 `;
 
 export default function Logo() {
   return (
-    <Link to="/">
-      <StyledLogo>
-        Mangaa
-      </StyledLogo>
-    </Link>
+    <StyledLogo to="/">
+      Mangaa
+    </StyledLogo>
   );
 }
